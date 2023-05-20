86cfc8188a8718b8e1b772aac3e6d58ad44f05af
mattiapomelli <mattiapomelli@gmail.com>
2023-05-20 12:45:19 +0200

    Improve UI for creating railgun wallet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
 import { NextPage } from "next";
 
-import { Button } from "@components/basic/button";
-import { useRailgun } from "@contexts/railgun-provider";
-
 const Home: NextPage = () => {
-  const { wallet, createWallet } = useRailgun();
-
-  return (
-    <div>
-      <p>{wallet?.zkAddress}</p>
-      <Button onClick={createWallet}>Create Railgun Wallet</Button>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
